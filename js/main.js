e742b34436c3f4712436bb48fc1e309240de54d5
wall-code <wtwallace2@gmail.com>
2023-04-01 16:05:19 -0300

    css do span

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+const date = new Date();
+
+const day = date.getDate()
+const month = date.getMonth()
+const year = date.getFullYear()
+
+
+
+const daysMonth = new Date (year, month + 1 , 0).getDate()
+
+
 function inicia(){
     
-    for(i=0;i<31;i++){
+    for(i=0;i<=daysMonth;i++){
         var dia =document.getElementById("conteiner");
         var span = document.createElement("span")
         span.textContent=i;
@@ -11,3 +22,12 @@ function inicia(){
 }
 
 window.addEventListener("load",inicia);
+
+
+/*const date = new Date();
+const mes=date.getMonth();
+const agora = new Date(date.getFullYear(),date.getMonth(),0).toString();
+
+console.log(agora)
+console.log(agora.substring(8,10));*/
+
