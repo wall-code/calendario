004be461afd009ce13c627bfcd687f7649df5e30
wall-code <wtwallace2@gmail.com>
2023-04-01 18:26:29 -0300

    bug do dom resolvido

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
+
+
+
 const date = new Date();
 var cont=1;
 const day = date.getDate()
 const month = date.getMonth()
 const year = date.getFullYear()
-var dia
 
 
-function load(){
-dias(cont)
-}
+ 
+    
 
     function dias(cont){
         const daysMonth = new Date (year, month + cont , 0).getDate()
         for(i=1;i<=daysMonth;i++){
-            //dia =document.getElementById("");
+            var dia =document.getElementById("conteiner");
+            console.log(dia)
             var span = document.createElement("span")
             span.textContent=i;
-           document.getElementById("conteiner").append(span)
-            
+           dia.append(span)
+           
+          
             
         }
         
-    }
+    } 
+
+
+dias(cont);
+
+
 function clickar(){
+   
     cont=cont+1;
     dias(cont);
     console.log("teste")
+    
 }
-load()
-/*const date = new Date();
-const mes=date.getMonth();
-const agora = new Date(date.getFullYear(),date.getMonth(),0).toString();
 
-console.log(agora)
-console.log(agora.substring(8,10));*/
 
