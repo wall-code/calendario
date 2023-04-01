40c769b19e1e7ab5da9ea7a636e75c278a70210b
wall-code <wtwallace2@gmail.com>
2023-04-01 19:06:00 -0300

    h1 com data

--- js/main.js
+++ js/main.js
@@ -6,35 +6,36 @@ var cont=1;
 const day = date.getDate()
 const month = date.getMonth()
 const year = date.getFullYear()
-
-
  
     
 
-    function dias(cont){
+    function dias(){
         const daysMonth = new Date (year, month + cont , 0).getDate()
+        
         for(i=1;i<=daysMonth;i++){
             var dia =document.getElementById("conteiner");
             console.log(dia)
             var span = document.createElement("span")
             span.textContent=i;
-           dia.append(span)
-           
-          
+           dia.appendChild(span)
             
         }
         
     } 
 
 
-dias(cont);
+
+dias();
 
 
 function clickar(){
-   
-    cont=cont+1;
-    dias(cont);
+    document.getElementById("conteiner").innerHTML=''
+    cont++;
+    dias();
     console.log("teste")
+    const datass = new Date (year, month + cont , 0).toString()
+    var formatada = datass.substring(4,7)+" "+datass.substring(11,15)
+    document.getElementById("data").innerHTML=formatada
     
 }
 
